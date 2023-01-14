c93b21a3fa7fb93fe3ac5045b4a70fbbf3d6c15b
Cristian Betivu <cbe@languagewire.com>
2023-01-14 14:25:21 +0200

    Various changes

--- project/kafka_consumer.py
+++ project/kafka_consumer.py
@@ -5,12 +5,21 @@
 # https://spark.apache.org/docs/3.3.1/structured-streaming-kafka-integration.html
 
 import json
+import math
 import os
+from collections import defaultdict
 
 import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import ArrayType, MapType, StringType, StructType
+from pyspark.sql.types import (
+    ArrayType,
+    BooleanType,
+    FloatType,
+    IntegerType,
+    StructType,
+    TimestampType,
+)
 
 os.environ[
     "PYSPARK_SUBMIT_ARGS"
@@ -31,13 +40,34 @@ kafka_message_schema = ArrayType(flight_schema)
 @F.udf(kafka_message_schema)
 def custom_json_converter(json_string: str):
     data = json.loads(json_string)
+    # dict keys are flight IDs; we probably should focus on aircraft ID later;
+    # values are tuples of:
+    # 0: aircraft number
+    # 1: latitute
+    # 2: longitude
+    # 3: heading (in degrees)
+    # 4: altitude (in feet)
+    # 5: speed relative to ground (in kts)
+    # 6: some empty string
+    # 7:
+    # 8: Aircraft model number
+    # 9: aircraft Registration number
+    # 10: sensor datapoint timestamp?
+    # 11: origin airport ID
+    # 12: destination airport ID
+    # 13: another ID, some identification number
+    # 14: number
+    # 15: number
+    # 16: callsign
+    # 17: number
+    # 18: airline icao (code?)
     return [
         {
             "flight_id": key,
             "aircraft_number": values[0],
             "latitude": values[1],
             "longitude": values[2],
-            "altitude": values[3],
+            "altitude": values[4],
         }
         for key, values in data.items()
         if key not in ("version", "full_count")
@@ -69,24 +99,67 @@ groupped_flights_df = (
 
 
 def compute_diff(group_key: tuple[dict, str], p_df: pd.DataFrame):
-    return pd.DataFrame(
-        {
-            "start": group_key[0]["start"],
-            "end": group_key[0]["end"],
-            "flight_id": group_key[1],
-            "lat_diff": p_df.latitude.diff(),
-            "long_diff": p_df.longitude.diff(),
-            "alt_diff": p_df.altitude.diff(),
-        }
+    # print("----------------------")
+    # print(group_key)
+    # print("----------------------")
+    # print(p_df)
+    p_df["lat_diff"] = p_df.latitude.diff()
+    p_df["long_diff"] = p_df.longitude.diff()
+    p_df["alt_diff"] = p_df.altitude.diff()
+    p_df["has_moved"] = (
+        (p_df.lat_diff != 0) | (p_df.long_diff != 0) | (p_df.alt_diff != 0)
     )
+    return p_df
 
 
-diff_df = groupped_flights_df.applyInPandas(
-    compute_diff,
-    schema="start timestamp, end timestamp, flight_id string, lat_diff float, long_diff float, alt_diff integer",
+flight_schema_with_diff = (
+    flight_schema.add("lat_diff", FloatType())
+    .add("long_diff", FloatType())
+    .add("alt_diff", IntegerType())
+    .add("timestamp", TimestampType())
+    .add("has_moved", BooleanType())
 )
 
-query2 = diff_df.dropna().filter(diff_df.lat_diff > 0).select("*")
+# https://insight-trucks.com/en/calculate_fuel_consumption/
+
+liters_per_km_by_engine_type = defaultdict(lambda: 100)
+
+
+def gcdist(lat1, lon1, lat2, lon2):
+    # Great Circle Distance Formula
+    # The radius in KM
+    R = 6378.137
+
+    # the formula requires we convert all degrees to radians
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    lon1 = math.radians(lon1)
+    lon2 = math.radians(lon2)
+
+    lat_span = lat1 - lat2
+    lon_span = lon1 - lon2
+
+    a = math.sin(lat_span / 2) ** 2
+    b = math.cos(lat1)
+    c = math.cos(lat2)
+    d = math.sin(lon_span / 2) ** 2
+
+    dist = 2 * R * math.asin(math.sqrt(a + b * c * d))
+
+    return dist
+
+
+diff_df = groupped_flights_df.applyInPandas(compute_diff, flight_schema_with_diff)
+
+# query2 = diff_df.dropna().filter(diff_df.lat_diff > 0).select("*")
+query2 = diff_df.dropna().filter(diff_df.flight_id == "2e61ed80").select("*")
 stream_query = query2.writeStream.outputMode("append").format("console").start()
 stream_query.awaitTermination(timeout=300)
 
+# (
+#     query2.writeStream.format("json")
+#     .option("path", "./output")
+#     .option("checkpointLocation", "./checkpoints")
+#     .start()
+#     .awaitTermination(timeout=120)
+# )
